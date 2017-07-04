606c32d4c3ef6413978a88812236debbfd2ae5c8
Maxim Nogin <maximnogin.dev@gmail.com>
2017-07-04 15:52:55 +0300

    added normolize

--- src/app/common-usage/validation-messages.ts
+++ src/app/common-usage/validation-messages.ts
@@ -3,7 +3,7 @@ export const validationMessages = {
   MinLength: 'Please, type more symbols',
   MaxLength: 'Please, remove several symbols',
   MinPhoneLength: 'Please, type full phone number',
-  ageValidator: 'You too much yang for this shit',
+  ageValidator: 'You too much yang for this',
   noMatch: "Password confirm doesn't match",
   requiredPlace: 'The place employment is required',
   symbolsValidator: 'At least one capital letter and number'
